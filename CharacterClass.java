5147f4e1ff75451c91ab82e78cc091484d9b9d58
Benji123abc <34463636+Benji123abc@users.noreply.github.com>
2018-11-09 17:42:16 -0500

    Add Basic Methods to CharacterClass

--- CharacterClass.java
+++ CharacterClass.java
@@ -1,4 +1,15 @@
 public class Character {
+  private String characterName;
 
-
+  
+  public void setName(String name) {
+   characterName = name; 
+  }
+  public String getName() {
+      return characterName;
+  }
+  
+  public Character (String name) {
+    characterName = name;
+  }
 }
