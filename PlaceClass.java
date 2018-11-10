105e0f5068c755bec5b5cf3afb89269cd0740229
Benji123abc <34463636+Benji123abc@users.noreply.github.com>
2018-11-10 08:58:13 -0500

    Add peopleInPlace to PlaceClass

--- PlaceClass.java
+++ PlaceClass.java
@@ -2,5 +2,11 @@ public class Place {
 	private String placeName;
 	private Character[] peopleInPlace;
 
-
+	public void addPersonInPlace(Character person) {
+		peopleInPlace.add(person);
+	}
+	
+	public Place(String name) {
+		placeName = name;
+	}
 }
